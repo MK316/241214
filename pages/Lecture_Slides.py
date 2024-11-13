f1349d1e875155f26e5acaf1f161e8365f58aea6
MK <99416359+MK316@users.noreply.github.com>
2024-11-13 18:14:49 +0900

    Update Lecture_Slides.py

--- pages/Lecture_Slides.py
+++ pages/Lecture_Slides.py
@@ -6,7 +6,7 @@ import os
 st.markdown("""
     <style>
     .stSelectbox div[data-baseweb="select"] {
-        margin-top: -5px;  /* Adjust this value to align with the buttons */
+        margin-top: -50px;  /* Adjust this value to align with the buttons */
     }
     </style>
     """, unsafe_allow_html=True)
