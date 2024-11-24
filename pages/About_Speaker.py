8610c610b117120b21c8f4ac1ddcb36ccb16a603
MK <99416359+MK316@users.noreply.github.com>
2024-11-24 23:05:14 +0900

    Update About_Speaker.py

--- pages/About_Speaker.py
+++ pages/About_Speaker.py
@@ -7,3 +7,5 @@ st.caption("https://englishedu.gnu.ac.kr")
 
 img_url = "https://github.com/MK316/241214/raw/main/image/speaker.png"
 st.image(img_url, use_column_width=True)
+
+st.write("Teaching courses: Digital Literacy & English Education (Spring 2023, Spring 2024)")
