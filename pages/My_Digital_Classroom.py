707a4a1e8b512a7578957ba170f15cef6a6a6c5e
MK <99416359+MK316@users.noreply.github.com>
2024-11-25 00:06:06 +0900

    Update My_Digital_Classroom.py

--- pages/My_Digital_Classroom.py
+++ pages/My_Digital_Classroom.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("My digital classroom")
+# JavaScript to automatically open a new tab
+def auto_open_url(url):
+    js = f"<script>window.open('{url}');</script>"
+    st.components.v1.html(js, height=0, width=0)
+
+# URL you want to open
+github_url = "https://github.com/yourusername/yourrepository"
+
+# Function call to open the URL automatically
+auto_open_url(github_url)
+
+st.title('Page Title')
+st.write("This page will automatically open a GitHub page when loaded.")
