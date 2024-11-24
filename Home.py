f6b880103900311239e078d0b63dcb1f28b45e70
MK <99416359+MK316@users.noreply.github.com>
2024-11-24 22:41:26 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,4 +6,4 @@ st.title("Main Page")
 st.write("Welcome to the main page of the app!")
 
 img_url = "https://github.com/MK316/241214/raw/main/image/bg02.png"
-st.image=(img_url, caption="\"In preparation\"", use_column_width=True)
+st.image(img_url, caption="\"In preparation\"", use_column_width=True)
