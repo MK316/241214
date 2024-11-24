0ddb39ab0ae2dea1e519f97f0801ba9d414b603f
MK <99416359+MK316@users.noreply.github.com>
2024-11-24 22:55:21 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,4 +7,4 @@ st.write("Symposium schedule: 1:30PM~3PM, 2024. 12. 14")
 st.caption("발표 관련 자료들이 업로드될 예정입니다.")
 
 img_url = "https://github.com/MK316/241214/raw/main/image/bg-02.png"
-st.image(img_url, caption="\"In preparation\"", use_column_width=True)
+st.image(img_url, caption="\"In preparation (Last updated: Nov.24)\"", use_column_width=True)
