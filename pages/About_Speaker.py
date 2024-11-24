4b18c95f2a93780c4577b055c8aef2861187b141
MK <99416359+MK316@users.noreply.github.com>
2024-11-24 23:00:46 +0900

    Update About_Speaker.py

--- pages/About_Speaker.py
+++ pages/About_Speaker.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.title("Page 1")
-st.write("This is the content of Page 1.")
+st.title("Presenter: ")
+st.write("Associate Professor at the Dept. of English Education, GNU")
+
+st.caption("https://englishedu.gnu.ac.kr")
+
+img_url = "https://github.com/MK316/241214/raw/main/image/speaker.png"
+st.image(img_url, use_column_width=True)
