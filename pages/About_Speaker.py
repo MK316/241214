ad20064f33c36a67594aaf428456dbb2ee148487
MK <99416359+MK316@users.noreply.github.com>
2024-11-24 23:06:55 +0900

    Update About_Speaker.py

--- pages/About_Speaker.py
+++ pages/About_Speaker.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 st.title("Presenter: ")
-st.markdown("### Associate Professor at the Dept. of English Education, GNU")
+st.markdown("+ Associate Professor at the Dept. of English Education, GNU")
 
 st.caption("https://englishedu.gnu.ac.kr")
 
 img_url = "https://github.com/MK316/241214/raw/main/image/speaker.png"
 st.image(img_url, use_column_width=True)
 
-st.markdown("### Teaching courses: Digital Literacy & English Education (Spring 2023, Spring 2024)")
+st.markdown("+ Teaching courses: Digital Literacy & English Education (Spring 2023, Spring 2024)")
