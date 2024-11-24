1faddc0079212edc3228cf6f0a132f38356e0d49
MK <99416359+MK316@users.noreply.github.com>
2024-11-25 00:41:36 +0900

    Update My_Digital_Classroom.py

--- pages/My_Digital_Classroom.py
+++ pages/My_Digital_Classroom.py
@@ -57,7 +57,7 @@ def main():
 
         st.markdown("+ [Github]('https://github.com/MK316')")
         img_url = "https://github.com/MK316/241214/raw/main/image/lady.png"
-        st.image(img_url, caption="It would've been wise to start coding back when I could actually see the screen without squinting... (to myself)", use_column_width=True)
+        st.image(img_url, width = 300, caption="It would've been wise to start coding back when I could actually see the screen without squinting... (to myself)", use_column_width=True)
 
 
 if __name__ == "__main__":
