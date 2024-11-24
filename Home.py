78841739acec5ee7b7950080e2c62eb1e74040be
MK <99416359+MK316@users.noreply.github.com>
2024-11-24 22:39:03 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,3 +4,6 @@ st.set_page_config(page_title="Welcome to Symposium session I")
 
 st.title("Main Page")
 st.write("Welcome to the main page of the app!")
+
+img_url = "https://github.com/MK316/241214/image/bg02.png"
+st.image=(img_url, caption="\"In preparation\"", use_column_width=True)
