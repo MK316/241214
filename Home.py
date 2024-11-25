2f5016f8fd7f8b3d7403acf702a984dfe678d172
MK <99416359+MK316@users.noreply.github.com>
2024-11-26 02:47:13 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,3 +8,6 @@ st.caption("발표 관련 자료들이 업로드될 예정입니다.")
 
 img_url = "https://github.com/MK316/241214/raw/main/image/bg-02.png"
 st.image(img_url, caption="\"In preparation (Last updated: Nov.24)\"", use_column_width=True)
+
+img_QR = "https://github.com/MK316/241214/blob/main/image/241214QR.jpg"
+st.image(img_QR, caption="QR code to link this page.", width=250)
