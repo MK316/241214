b1d89caacc57f325b0e4f71e7b02ae82fa8fade5
MK <99416359+MK316@users.noreply.github.com>
2024-11-26 00:15:55 +0900

    Update About_Speaker.py

--- pages/About_Speaker.py
+++ pages/About_Speaker.py
@@ -5,11 +5,8 @@ img_url = "https://github.com/MK316/241214/raw/main/image/speaker.png"
 st.image(img_url, use_column_width=True)
 st.caption("https://englishedu.gnu.ac.kr")
 
-st.markdown("+ Associate Professor at the Dept. of English Education, GNU")
-
 st.markdown("+ Interested in digital literacy training for prospective English teachers")
 st.caption("Digital Literacy & English Education (Spring 2023, Spring 2024)")
-
 img_url2 = "https://github.com/MK316/241214/raw/main/image/KeywordCloud_231129.png"
 st.image(img_url2, use_column_width=True)
 st.markdown("+ **My Digital Literacy Education keywords:**")
