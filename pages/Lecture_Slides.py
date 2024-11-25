e86a809c934a1805bda0ef86d1bc66065e0dde76
MK <99416359+MK316@users.noreply.github.com>
2024-11-26 00:00:51 +0900

    Update Lecture_Slides.py

--- pages/Lecture_Slides.py
+++ pages/Lecture_Slides.py
@@ -34,7 +34,7 @@ def display_image():
     st.image(resized_image, caption=f"Slide {st.session_state.slide_index + 1} of {num_slides}")
 
 # Create tabs
-tab1, tab2 = st.tabs(["🔲 Slides", "🔲 Videos"])
+tab1, tab2 = st.tabs(["🌱 Slides", "🌱 Videos"])
 
 with tab1:
 
