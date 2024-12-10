d554cc84445615f6f91ed64720903dc1051d3b97
MK <99416359+MK316@users.noreply.github.com>
2024-12-10 15:43:21 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Welcome to Symposium session I")
 
 st.title("2024 제1회 경상 디지털 교육 나눔 한마당")
-st.caption("주최: 경상국립대학교 사범대학"
+st.caption("주최: 경상국립대학교 사범대학")
 st.write("💦 Symposium schedule: 1:30PM~3PM, 2024. 12. 14")
 st.caption("발표 관련 자료들이 업로드될 예정입니다.")
 
