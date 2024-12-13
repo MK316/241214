54085da24f482c8b27efedb8dfcb0ee0a3fa86ce
MK <99416359+MK316@users.noreply.github.com>
2024-12-14 01:09:32 +0900

    Update About_Speaker.py

--- pages/About_Speaker.py
+++ pages/About_Speaker.py
@@ -2,13 +2,15 @@ import streamlit as st
 
 st.title("Presenter: ")
 img_url = "https://github.com/MK316/241214/raw/main/image/speaker.png"
-st.image(img_url, use_column_width=True)
+# Updated to use the new parameter
+st.image(img_url, use_container_width=True)
 st.caption("https://englishedu.gnu.ac.kr")
 
 st.markdown("+ Interested in digital literacy training for prospective English teachers")
 st.caption("Digital Literacy & English Education (Spring 2023, Spring 2024)")
 img_url2 = "https://github.com/MK316/241214/raw/main/image/KeywordCloud_231129.png"
-st.image(img_url2, use_column_width=True)
+# Updated to use the new parameter
+st.image(img_url2, use_container_width=True)
 st.markdown("+ **My Digital Literacy Education keywords:**")
 st.markdown("1. **Independent Digital Competency**")
 st.markdown("2. **Self-sufficient Teaching Tools**")
