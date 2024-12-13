971a347b8480fb42eb8a572a7f7cede460155268
MK <99416359+MK316@users.noreply.github.com>
2024-12-14 00:56:05 +0900

    Update 🔵_Lesson_Plans.py

--- pages/🔵_Lesson_Plans.py
+++ pages/🔵_Lesson_Plans.py
@@ -8,10 +8,10 @@ st.write("Explore a collection of English class lesson plans created by students
 
 # List of lesson plans with links to open in a new tab
 lesson_plans = {
-    "Lesson Plan 1": "https://github.com/psy03",
-    "Lesson Plan 2": "https://github.com/MsMc24/G1-finalproject/blob/main/README.md",
-    "Lesson Plan 3": "https://github.com/ShieldEdu/G4-finalproject/blob/main/README.md",
-    "Lesson Plan 4": "https://github.com/verastudio/G2-finalproject/blob/main/README.md",
+    "💠 Lesson Plan 1": "https://github.com/psy03",
+    "💠 Lesson Plan 2": "https://github.com/MsMc24/G1-finalproject/blob/main/README.md",
+    "💠 Lesson Plan 3": "https://github.com/ShieldEdu/G4-finalproject/blob/main/README.md",
+    "💠 Lesson Plan 4": "https://github.com/verastudio/G2-finalproject/blob/main/README.md",
 }
 
 # Display the list of lesson plans as links
