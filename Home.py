995472160c882e9e83f477b026d8f9ef9992cb7e
MK <99416359+MK316@users.noreply.github.com>
2024-12-14 00:59:46 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,8 @@ st.write("💦 Symposium schedule: 1:00PM~2:30PM, 2024. 12. 14")
 st.caption("발표 관련 자료들이 업로드될 예정입니다.")
 
 img_url = "https://github.com/MK316/241214/raw/main/image/bg-02.png"
-st.image(img_url, caption="\"In preparation (Last updated: Nov.24)\"", use_column_width=True)
+# Updated parameter to use_container_width=True
+st.image(img_url, caption="\"In preparation (Last updated: Nov.24)\"", use_container_width=True)
 
 img_QR = "https://github.com/MK316/241214/raw/main/image/241214QR.jpg"
-st.image(img_QR, caption="QR to HERE :-)", width=100)
+st.image(img_QR, caption="QR to HERE :-)", width=200)
